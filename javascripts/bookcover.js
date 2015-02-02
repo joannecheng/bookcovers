c9f18214a8abaccabf6e3b3e6a1bd9591c486ca0
Joanne Cheng <joanne@thoughtbot.com>
2015-02-02 16:14:34 -0700

    Automated commit at 2015-02-02 23:14:34 UTC by middleman-deploy 3.3.7

---
+++ javascripts/bookcover.js
@@ -0,0 +1,27 @@
+(function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  window.Bookcover = (function() {
+    function Bookcover(_at_canvasID) {
+      this.canvasID = _at_canvasID;
+      this.draw = __bind(this.draw, this);
+      this.width = __bind(this.width, this);
+      this.height = __bind(this.height, this);
+      console.log(this.height());
+    }
+
+    Bookcover.prototype.height = function() {
+      return $("#" + this.canvasID).height();
+    };
+
+    Bookcover.prototype.width = function() {
+      return $("#" + this.canvasID).width();
+    };
+
+    Bookcover.prototype.draw = function() {};
+
+    return Bookcover;
+
+  })();
+
+}).call(this);
--- javascripts/haskell_cover.js
+++ javascripts/haskell_cover.js
@@ -1,4 +1,27 @@
 (function() {
-  console.log("hello!");
+  var HaskellCover, h,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    __hasProp = {}.hasOwnProperty;
+
+  HaskellCover = (function(_super) {
+    __extends(HaskellCover, _super);
+
+    function HaskellCover() {
+      this.draw = __bind(this.draw, this);
+      return HaskellCover.__super__.constructor.apply(this, arguments);
+    }
+
+    HaskellCover.prototype.draw = function() {
+      return console.log("drawing");
+    };
+
+    return HaskellCover;
+
+  })(Bookcover);
+
+  h = new HaskellCover("book_cover");
+
+  h.draw();
 
 }).call(this);
